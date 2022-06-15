4e5f494b6bd267826252c844125268b9bbf7dd2f
Mark Grimm <mark.grimm.coding@gmail.com>
2022-06-15 15:24:59 +0200

    finished, ball stops edge , starts again on click

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
+let x = 0;
+let y = 100;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(200, 200);
 }
-
+function mousePressed() {
+  x = 0;
+}
 function draw() {
   background(220);
+  circle(x, y, 30);
+  if (x < 200) {
+    x = x + 1;
+  }
 }
